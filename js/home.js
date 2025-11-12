fd33f93756c1abcaa0447d87457d5e6fe4160287
Nahuel-Floress <nahuelflores56789@gmail.com>
2025-11-12 15:13:03 -0300

    subo cambios

--- js/home.js
+++ js/home.js
@@ -1,1 +1,12 @@
-/* EN PROCESO */
+const bannerImg = document.querySelector('.dlc-banner img');
+const galeriaImgs = document.querySelectorAll('.dlc-galeria img');
+
+galeriaImgs.forEach(img => {
+  img.addEventListener('click', () => {
+    const tempSrc = bannerImg.src;
+    const newSrc = img.src;
+
+    bannerImg.src = newSrc;
+    img.src = tempSrc;
+  });
+});
